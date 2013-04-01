25e07523a267a07f605734a37c24d87e085a2d1b
damgad <damgad@gmail.com>
2013-04-01 17:30:24 +0200

    divided to model and controller, deleted test method (to calculate accurate nb of threads)

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,16 +2,15 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import models._
 import java.util.concurrent._
 import org.springframework.core.env.SystemEnvironmentPropertySource
 
 object Application extends Controller {
 
-  val executor = new ThreadPoolExecutor(20, 40, 60, TimeUnit.SECONDS, new LinkedBlockingQueue)
-
   def newTask(classpath:String) = Action {
     try {
-      executor.execute(Class.forName(classpath).newInstance.asInstanceOf[Runnable])
+      Scheduler.putTask(Class.forName(classpath).newInstance.asInstanceOf[Runnable])
       Ok
     } catch {
       case e @ (_ : ClassCastException | _ :  InstantiationException | _ : ClassNotFoundException | _ : IllegalAccessException) => println(e+" (returned 400)"); BadRequest
@@ -19,29 +18,8 @@ object Application extends Controller {
     }
   }
 
-  def scheduled = Action { Ok(executor.getQueue.size.toString) }
+  def scheduled = Action { Ok(Scheduler.getScheduledNb.toString) }
 
-  def running = Action { Ok(executor.getActiveCount.toString) }
-
-  def setthreads(classpath:String) = Action {
-
-    val task = Class.forName(classpath).newInstance.asInstanceOf[Runnable];
-    def nextTest(nbthreads:Int, lasttimepertask:Double) :Int ={
-      executor.setCorePoolSize(nbthreads)
-      executor.setMaximumPoolSize(2*nbthreads)
-      //System.out.print("Test for" + nbthreads)
-      val s= System.nanoTime
-      for(j <- 1 to nbthreads-1)  executor.execute(task)
-      executor.submit(task).get
-      val timepertask : Double= ((System.nanoTime-s))/nbthreads
-      //System.out.println(": " +timepertask)
-      if ( timepertask > lasttimepertask) nbthreads  else nextTest(nbthreads*2,timepertask)
-
-    }
-    val nb=nextTest(10,Double.MaxValue)
-    executor.setCorePoolSize(3*nb/4);
-    executor.setMaximumPoolSize(nb)
-    Ok((3*nextTest(10,Double.MaxValue)/4).toString)
-  }
+  def running = Action { Ok(Scheduler.getRunningNb.toString) }
 
 }
