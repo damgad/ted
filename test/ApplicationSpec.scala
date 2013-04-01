d1b1739d83ff396753c0f631341a638fc63835fa
damgad <damgad@gmail.com>
2013-04-01 19:26:39 +0200

    added all request cases

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -4,29 +4,55 @@ import org.specs2.mutable._
 
 import play.api.test._
 import play.api.test.Helpers._
+import play.api.test.FakeApplication
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
- */
+
 class ApplicationSpec extends Specification {
   
   "Application" should {
     
     "send 404 on a bad request" in {
       running(FakeApplication()) {
-        route(FakeRequest(GET, "/boum")) must beNone        
+        val result = route(FakeRequest(GET, "/"))
+        result must beNone
       }
     }
-    
-    "render the index page" in {
+
+    "send 200 on a normal, correct task schedule request" in {
       running(FakeApplication()) {
-        val home = route(FakeRequest(GET, "/")).get
-        
-        status(home) must equalTo(OK)
-        contentType(home) must beSome.which(_ == "text/html")
-        contentAsString(home) must contain ("Your new application is ready.")
+        val result = route(FakeRequest(GET, "/tasks/schedule?class=example.Itemize")).get
+        status(result) must equalTo(OK)
+        contentAsString(result) must beEmpty
+      }
+    }
+    "send 400 on a wrong task name schedule request" in {
+      running(FakeApplication()) {
+        val result = route(FakeRequest(GET, "/tasks/schedule?class=NotExist")).get
+        status(result) must equalTo(BAD_REQUEST)
+        contentAsString(result) must beEmpty
+      }
+    }
+    "send 400 on a non-runnable task schedule request" in {
+      running(FakeApplication()) {
+        val result = route(FakeRequest(GET, "/tasks/schedule?class=test.ApplicationSpec")).get
+        status(result) must equalTo(BAD_REQUEST)
+        contentAsString(result) must beEmpty
+      }
+    }
+
+    "send 200 and number on a count scheduled request" in {
+      running(FakeApplication()) {
+        val result = route(FakeRequest(GET, "/tasks/count-scheduled")).get
+        status(result) must equalTo(OK)
+        contentAsString(result).toInt must be_>=(0)
+      }
+    }
+
+    "send 200 and number on a count running request" in {
+      running(FakeApplication()) {
+        val result = route(FakeRequest(GET, "/tasks/count-scheduled")).get
+        status(result) must equalTo(OK)
+        contentAsString(result).toInt must be_>=(0)
       }
     }
   }
